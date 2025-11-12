1d3d44649ee5f03625d954bc20ff2a0ce391f183
Fabio Irimie <fabioirimie@gmail.com>
2025-11-12 22:01:35 +0100

    feat: completed texture chapter

---
+++ textures/src/shaders/vertex.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 color;
+layout (location = 2) in vec2 texture;
+
+out vec4 vertex_color;
+out vec2 texture_coords;
+
+void main() {
+  gl_Position = vec4(pos, 1.0);
+  vertex_color = vec4(color, 1.0);
+  texture_coords = texture;
+}
